d10059e20f8f6e96a9b7b3576f3963d5781071d1
Kyrylo Fedorov <kyryloFedorov@gmail.com>
2015-12-09 19:13:43 +0200

    test for airbrake.io

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,18 +4,72 @@
  */
 var APP_NAME = 'threeBoar';
 
-(function(angular, window){
+(function (angular, window) {
 	var app = angular.module(window.APP_NAME, ['threeBoar.indicator']);
 
-	app.run([function(){
+	app.constant('airBrakeConfig', {
+		projectId: 118406,
+		projectKey: '0b5344a3a7679f2981ed4c73b44c99f6'
+	});
+
+	//var airbrake = new airbrakeJs.Client({projectId: 118406, projectKey: '0b5344a3a7679f2981ed4c73b44c99f6'});
+	//if (window.jQuery) {
+	//	airbrakeJs.instrumentation.jquery(airbrake, jQuery);
+	//}
+
+	app.factory('$exceptionHandler', addAirBrakeHandler);
+	app.factory('airBrake', airbrakeServiceFactory);
+
+
+	app.run([function () {
 
 		console.log('app:run::%s::Test application  "%s" running.', new Date(), window.APP_NAME);
+
+		//try {
+		//	 This will throw if the document has no head tag
+		//	document.head.insertBefore(document.createElement("style"));
+		//} catch(err) {
+		//	airbrake.push(err);
+		//	throw err;
+		//}
+
 	}]);
+
+
+	addAirBrakeHandler.$inject = ['$log', 'airBrakeConfig'];
+
+	function addAirBrakeHandler($log, config) {
+		var airbrake = new airbrakeJs.Client({
+			projectId: config.projectId,
+			projectKey: config.projectKey
+		});
+
+		airbrake.addFilter(function (notice) {
+			notice.context.environment = 'testing';
+			return notice;
+		});
+
+		return function (exception, cause) {
+			$log.error(exception);
+			airbrake.notify({error: exception, params: {angular_cause: cause}});
+		};
+	}
+
+	airbrakeServiceFactory.$inject = ['airBrakeConfig'];
+
+	function airbrakeServiceFactory(config) {
+		var airbrake = new airbrakeJs.Client({
+			projectId: config.projectId,
+			projectKey: config.projectKey
+		});
+
+		return airbrake;
+	}
 
 })(window.angular, window);
 
 
-;(function(angular, window){
+;(function (angular, window) {
 	var MODULE_NAME = window.APP_NAME + '.indicator';
 	var module = angular.module(MODULE_NAME, []);
 
@@ -24,10 +78,57 @@ var APP_NAME = 'threeBoar';
 	 */
 	module.controller('indicatorController', IndicatorControllerFactory);
 
-	IndicatorControllerFactory.$inject = ['$scope'];
-	function IndicatorControllerFactory($scope){
+	IndicatorControllerFactory.$inject = ['$scope', 'airBrake', '$http'];
+
+	function IndicatorControllerFactory($scope, airBrake, $http) {
 		console.log(MODULE_NAME, 'indicatorController', 'run factory');
 
 		$scope.message = 'Hello log collectors';
+
+		$scope.throwException = throwException;
+		$scope.useInvalidDate = useInvalidDate;
+		$scope.makeReferenceError = makeReferenceError;
+		$scope.requestToNothing = requestToNothing;
+		$scope.doUnAuthRequest = unAuthRequest;
+
+		airBrake.notify('Just try to send notify via ngService');
+
+		function throwException() {
+			throw new Error('Testing airbrake:: It\'s throw thrown ng.$exceptionHandler');
+		}
+
+		function useInvalidDate() {
+			$scope.message = 'Now try set invalid date' + new Date('сегодня');
+		}
+
+		function makeReferenceError() {
+			$scope.message = hello;
+
+		}
+
+		function requestToNothing() {
+			$http.post('//example.com', {nothing: null}).then(
+				function (response) {
+					console.warn('Невозможный запрос выполнен успешно');
+				});
+		//,
+		//		function (error) {
+		//			console.error('Error on request to nothing, as expected');
+		//		}
+		//	)
+		}
+
+		function unAuthRequest(){
+			$http.get('https://dev.quickestate.ca/dev-back/step').then(
+				function(response){
+					console.warn('Unauth request passed');
+				},
+				function(error){
+					console.error(error);
+				}
+			)
+		}
 	}
+
+
 })(window.angular, window);
